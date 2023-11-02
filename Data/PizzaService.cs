dfd249a061f11bb3c6c54eccaed362f4c8f6391f
Waqar Kabir <waqarkabi@hotmail.com>
2023-11-02 21:58:12 +0500

    Blazor App With SQLite

---
+++ Data/PizzaService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazingPizza.Data
+{
+    public class PizzaService
+    {
+        public Task<Pizza[]> GetPizzasAsync() 
+        {
+            //Todo: Get Data from Data Source
+            return null;
+        }
+    }
+}
